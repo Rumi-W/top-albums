76b551f4dfbf1f669f0fca1d20ca6c2c8ce1963e
Harumi Warner <harumi@harumis-mbp.home>
2020-04-12 10:18:47 -0400

    Updates

---
+++ precache-manifest.d005891eb48967f57b7311083175767e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c277a0b3f8ea1aae22d1fc68bb4126aa",
+    "url": "/top-albums/index.html"
+  },
+  {
+    "revision": "1c3adbd1dd5e5c18193c",
+    "url": "/top-albums/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "c16cb1956447065aac83",
+    "url": "/top-albums/static/css/main.9b249a91.chunk.css"
+  },
+  {
+    "revision": "1c3adbd1dd5e5c18193c",
+    "url": "/top-albums/static/js/2.e29af680.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/top-albums/static/js/2.e29af680.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c16cb1956447065aac83",
+    "url": "/top-albums/static/js/main.40e3f21a.chunk.js"
+  },
+  {
+    "revision": "4c5d4bc12bf126ae1886",
+    "url": "/top-albums/static/js/runtime-main.1e24d648.js"
+  },
+  {
+    "revision": "1198b1c414ad8610f8ceca0dda783c09",
+    "url": "/top-albums/static/media/cover.1198b1c4.jpg"
+  }
+]);
