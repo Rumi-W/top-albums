3411376c8375f9438107050217c57e6054715bc6
Harumi Warner <harumi@harumis-mbp.home>
2020-04-12 05:00:25 -0400

    Updated Readme. Adjusted the URL for the home route.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css'
 class App extends PureComponent {
   render() {
     return (
-      <BrowserRouter>
+      <BrowserRouter basename="/top-albums/">
         <Switch>
           <Route path="/top100">
             <Wrapper />
